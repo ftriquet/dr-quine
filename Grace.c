ce1e57a57903f422a0a5e1bc64677ae1f24fe79a
François Triquet <francois.triquet@recast.ai>
2016-09-04 18:02:56 +0200

    add comment to grace

--- Grace.c
+++ Grace.c
@@ -3,5 +3,8 @@
 #include <fcntl.h>
 #define FLAGS O_WRONLY|O_CREAT|O_TRUNC
 #define NAME "Grace_kid.c"
-#define FT(x) int main(void){char *str="#include <stdio.h>%c#include <unistd.h>%c#include <fcntl.h>%c#define FLAGS O_WRONLY|O_CREAT|O_TRUNC%c#define NAME %cGrace_kid.c%c%c#define FT(x) int main(void){char *str=%c%s%c;int fd=open(x,FLAGS,0644);dprintf(fd,str, 10, 10, 10, 10, 34, 34, 10, 34, str, 34, 10, 10);close(fd);}%cFT(NAME)%c";int fd=open(x,FLAGS,0644);dprintf(fd,str, 10, 10, 10, 10, 34, 34, 10, 34, str, 34, 10, 10);close(fd);}
+#define FT(x) int main(void){char *str="#include <stdio.h>%c#include <unistd.h>%c#include <fcntl.h>%c#define FLAGS O_WRONLY|O_CREAT|O_TRUNC%c#define NAME %cGrace_kid.c%c%c#define FT(x) int main(void){char *str=%c%s%c;int fd=open(x,FLAGS,0644);dprintf(fd,str, 10, 10, 10, 10, 34, 34, 10, 34, str, 34, 10, 10, 10, 10, 10);close(fd);}%c/*%c	Lache tes coms%c*/%cFT(NAME)%c";int fd=open(x,FLAGS,0644);dprintf(fd,str, 10, 10, 10, 10, 34, 34, 10, 34, str, 34, 10, 10, 10, 10, 10);close(fd);}
+/*
+	Lache tes coms
+*/
 FT(NAME)
