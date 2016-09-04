572fee601003683128090b1e76de5090e8cb6de9
François Triquet <francois.triquet@recast.ai>
2016-09-04 21:58:46 +0200

    test script

--- test.sh
+++ test.sh
@@ -1,36 +1,73 @@
 #!/bin/sh
 
-if [ -f Colleen.c ]; then
-	echo "Testing Colleen:"
-	gcc -Wall -Wextra -Werror Colleen.c -o colleen
-	df=`./colleen | diff - Colleen.c`
-	if [ -z  $df ]; then
-		echo "\x1b[32mOK\x1b[0m"
-	else
-		echo "\x1b[31mFAIL\x1b[0m"
-		echo "diff:"
-		echo $df
-	fi
-	rm -f colleen
-	echo ""
-fi
-
-if [ -f Grace.c ]; then
-	echo "Testing Grace:"
-	gcc -Wall -Wextra -Werror Grace.c -o grace
-	./grace
-	if [ -f Grace_kid.c ]; then
-		df=`cat Grace_kid.c | diff - Grace.c`
-		if [ -z $df ]; then
+test_colleen() {
+	if [ -f Colleen.c ]; then
+		echo "Testing Colleen:"
+		gcc -Wall -Wextra -Werror Colleen.c -o colleen
+		df=`./colleen | diff - Colleen.c`
+		if [ -z  $df ]; then
 			echo "\x1b[32mOK\x1b[0m"
 		else
 			echo "\x1b[31mFAIL\x1b[0m"
 			echo "diff:"
 			echo $df
 		fi
+		rm -f colleen
 	else
-		echo "Fail. grace_kid.c was not created"
+		echo "\x1b[31mColleen.c doesn't exist\x1b[0m"
 	fi
-	rm -f grace Grace_kid.c
 	echo ""
-fi
+}
+
+test_grace() {
+	if [ -f Grace.c ]; then
+		echo "Testing Grace:"
+		gcc -Wall -Wextra -Werror Grace.c -o grace
+		./grace
+		if [ -f Grace_kid.c ]; then
+			df=`cat Grace_kid.c | diff - Grace.c`
+			if [ -z $df ]; then
+				echo "\x1b[32mOK\x1b[0m"
+			else
+				echo "\x1b[31mFAIL\x1b[0m"
+				echo "diff:"
+				echo $df
+			fi
+		else
+			echo "Fail. grace_kid.c was not created"
+		fi
+		rm -f grace Grace_kid.c
+	else
+		echo "\x1b[31mGrace.c doesn't exist\x1b[0m"
+	fi
+	echo ""
+}
+
+
+test_sully() {
+	if [ -f Sully.c ]; then
+		echo "Testing Sully:"
+		gcc -Wall -Wextra -Werror Sully.c -o Sully
+		./Sully
+		for bin in Sully_5 Sully_4 Sully_3 Sully_2 Sully_1 Sully_0; do
+			if [ -f $bin ] && [ -f $bin.c ]; then
+				df=`diff Sully.c $bin.c`
+				echo "Diff Sully.c <-> $bin.c"
+				echo $df
+				echo "-----------------------"
+				rm -f $bin $bin.c
+			else
+				echo "\x1b[31mFAIL: $bin doesn't exist\x1b[0m"
+				return
+			fi
+		done
+	else
+		echo "\x1b[31mSully.c doesn't exist\x1b[0m"
+	fi
+	rm -f Sully
+	echo ""
+}
+
+test_colleen
+test_grace
+test_sully
