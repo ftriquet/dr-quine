7ad7421f5d6283c3cb48cb17526650f1e851f20f
François Triquet <francois.triquet@recast.ai>
2016-09-04 20:49:16 +0200

    end sully

--- Sully.c
+++ Sully.c
@@ -4,8 +4,11 @@
 #include <fcntl.h>
 #define FLAGS O_WRONLY|O_CREAT|O_TRUNC
 int main(void){
-int i=12;--i;char *filename;asprintf(&filename,"Sully_%d.c",i);int fd=open(filename,FLAGS,0644);
-char *str="#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <fcntl.h>%c#define FLAGS O_WRONLY|O_CREAT|O_TRUNC%cint main(void){%cint i=%d;--i;char *filename;asprintf(&filename,%cSully_%cd.c%c,i);int fd=open(filename,FLAGS,0644);char *str=%c%s%c;dprintf(fd,str,10,10,10,10,10,10,i,34,37,34,34,str,34,10,10,34,37,34,34,34,10);%cclose(fd);%cchar *command;asprintf(&command,%cgcc -Wall -Wextra -Werror Sully_%cd.c -o sully%c,i);system(command);system(%c./sully%c);return 0;%c}";
-dprintf(fd,str,10,10,10,10,10,10,i,34,37,34,34,str,34,10,10,34,37,34,34,34,10);
-close(fd);char *command;asprintf(&command,"gcc -Wall -Wextra -Werror Sully_%d.c -o sully",i);system(command);system("./sully");return 0;
+int i=5;
+if(i--==0){return 0;};char *filename;asprintf(&filename,"Sully_%d.c",i);int fd=open(filename,FLAGS,0644);
+char *binary;asprintf(&binary, "./Sully_%d",i);
+char *command;asprintf(&command,"gcc -Wall -Wextra -Werror %s -o %s",filename,binary);
+char *str="#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <fcntl.h>%c#define FLAGS O_WRONLY|O_CREAT|O_TRUNC%cint main(void){%cint i=%d;%cif(i--==0){return 0;};char *filename;asprintf(&filename,%cSully_%cd.c%c,i);int fd=open(filename,FLAGS,0644);%cchar *binary;asprintf(&binary,%c./Sully_%cd%c,i);%cchar *command;asprintf(&command,%cgcc -Wall -Wextra -Werror %cs -o %cs%c,filename,binary);%cchar *str=%c%s%c;dprintf(fd,str,10,10,10,10,10,10,i,10,34,37,34,10,34,37,34,10,34,37,37,34,10,34,str,34,10,10,10);%cclose(fd);system(command);system(binary);return 0;%c}%c";
+dprintf(fd,str,10,10,10,10,10,10,i,10,34,37,34,10,34,37,34,10,34,37,37,34,10,34,str,34,10,10,10);
+close(fd);system(command);system(binary);return 0;
 }
